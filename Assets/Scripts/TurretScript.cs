89442d438e0d9a58df738e4725f8443835f256b2
ErwanGF <erwangf@gmail.com>
2016-07-15 19:43:18 +0400

    modified

---
+++ Assets/Scripts/TurretScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretScript : MonoBehaviour {
+
+
+	
+	public GameObject target_object;
+    private Vector3 targetDirection;
+
+    public Rigidbody RB; //the turret RigidBody
+
+    void Start()
+    {
+        RB = GetComponent<Rigidbody>();
+    }
+
+	void Update () {
+
+	}
+
+	void FixedUpdate () {
+
+        targetDirection = target_object.transform.position - RB.transform.position;
+        
+		RB.transform.rotation= Quaternion.LookRotation(targetDirection);	//rotate in direction of the target
+
+	}
+}
